141116f25e1a15d9415002766f72c92492fe937b
Stefan Braicu <sbraicu@cisco.com>
2024-04-20 01:32:23 -0700

    initial commit

---
+++ init/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#setupcluster <number of clusters> <number of cp> <number of workers> <k8s version>
+# - for example creating 1 cluster with 1 CP and 2 workers using k8s v1.24.15:
+#setupcluster 1 1 2 1.24.15
